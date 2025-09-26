90d2fbb9fa2485acf0db2effde02e55eeba75565
Alecsander <alecsanderdias@hotmail.com>
2025-09-25 23:59:41 -0300

    Adicionando Service

--- Javascript/GraphQL/index.js
+++ Javascript/GraphQL/index.js
@@ -1,17 +1,7 @@
-import express from 'express';
-import { createHandler } from 'graphql-http/lib/use/express';
-import { schema } from './schema.js';
-import { root } from './resolvers.js';
- 
+import { app } from "./app.js";
+
 const PORT = 4000;
-const app = express();
-app.all(
-  '/graphql',
-  createHandler({
-    schema: schema,
-    rootValue: root,
-  }),
-);
+
 app.listen(PORT, () => {
   console.log('Running a GraphQL API server at localhost:4000/graphql');
 });
