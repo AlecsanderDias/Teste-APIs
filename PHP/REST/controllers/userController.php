8e56f2f51b9964e9c4032e363c19049911383ebc
Alecsander <alecsanderdias@hotmail.com>
2025-10-16 23:58:39 -0300

    Criando crud User

---
+++ PHP/REST/controllers/userController.php
@@ -0,0 +1,30 @@
+<?php 
+class UserController {
+    public function index(): void {
+
+    }
+
+    public function show(string $id): void {
+
+    }
+
+    public function create(): void {
+
+    }
+
+    public function store(): void {
+
+    }
+
+    public function edit(string $id): void {
+
+    }
+
+    public function update(string $id): void {
+
+    }
+
+    public function delete(string $id): void {
+
+    }
+}
--- PHP/REST/index.php
+++ PHP/REST/index.php
@@ -9,8 +9,12 @@ $dir = dirname(__DIR__);
 $dotenv = Dotenv\Dotenv::createImmutable($dir);
 $dotenv->safeLoad();
 
+$connection = "mysql:host=" + $_ENV['DB_HOST']+ ";dbname="+ $_ENV['DB_DATABASE'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+
 Flight::register('db', PdoWrapper::class, [
-    'mysql:host=localhost;dbname=prisma', 'root', 'root', [
+    $connection, $user, $pass, [
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'utf8mb4\'',
         PDO::ATTR_EMULATE_PREPARES => false,
         PDO::ATTR_STRINGIFY_FETCHES => false,
@@ -19,15 +23,17 @@ Flight::register('db', PdoWrapper::class, [
 ]);
 
 
-Flight::route('/', function() {
-    $users = Flight::db()->fetchAll("SELECT * FROM users;");
-    foreach ($users as $user) {
-        echo $user->name;
-    }
-    Flight::json([
-        'message' => 'Grande sucesso',
-        'Resultado' => $users
-    ]);
-});
+// Flight::route('/users', function() {
+//     $users = Flight::db()->fetchAll("SELECT * FROM users;");
+//     foreach ($users as $user) {
+//         echo $user->name;
+//     }
+//     Flight::json([
+//         'message' => 'Grande sucesso',
+//         'Resultado' => $users
+//     ]);
+// });
+
+Flight::resource('/user', UserController::class);
 
 Flight::start();
